44b93b9ad19017c609e32468188260c2f28580fe
Tom <your.email@example.com>
2025-02-13 00:19:09 +0700

    Initial commit

---
+++ src/main/java/com/nw/internbu3/InternBu3Application.java
@@ -0,0 +1,13 @@
+package com.nw.internbu3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternBu3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternBu3Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/nw/internbu3/hw1/Product.java
@@ -0,0 +1,53 @@
+package com.nw.internbu3.hw1;
+
+public class Product {
+    private String name;
+    private double price;
+    private String category;
+    private int stock;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    // Phương thức hiển thị thông tin sản phẩm
+    public void displayProductInfo() {
+        System.out.println("Product Information:");
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price +" đ");
+        System.out.println("Category: " + category);
+        System.out.println("Stock: " + stock);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
