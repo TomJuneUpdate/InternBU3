8e3f0b433bd58019b499915c569261b60f50d09a
Tom <nhdoma02@gmail.com>
2025-02-13 16:30:24 +0700

    add

--- src/main/java/com/nw/internbu3/hw1/Admin.java
+++ src/main/java/com/nw/internbu3/hw1/Admin.java
@@ -1,7 +1,16 @@
 package com.nw.internbu3.hw1;
 
+import java.util.List;
+
 public class Admin extends User {
+    private List<String> permissions;
+
     public Admin(String username, String email, String password) {
         super(username, email, password);
+        this.permissions = List.of("MANAGE_PRODUCTS", "MANAGE_ORDERS");
+    }
+
+    public List<String> getPermissions() {
+        return permissions;
     }
 }
--- src/main/java/com/nw/internbu3/hw1/Customer.java
+++ src/main/java/com/nw/internbu3/hw1/Customer.java
@@ -1,7 +1,16 @@
 package com.nw.internbu3.hw1;
 
+import java.util.List;
+
 public class Customer extends User{
+    private List<String> permissions;
+
     public Customer(String username, String email, String password) {
         super(username, email, password);
+        this.permissions = List.of("BUY_PRODUCTS", "WRITE_REVIEWS");
+    }
+
+    public List<String> getPermissions() {
+        return permissions;
     }
 }
